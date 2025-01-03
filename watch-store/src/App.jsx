c5f70be6977af47ce29e600c1e86c49d82940004
Dthangac <vuducthang20011@gmail.com>
2025-01-03 09:59:06 +0700

    test2test2

--- watch-store/src/App.jsx
+++ watch-store/src/App.jsx
@@ -1,15 +1,23 @@
-// import { useState } from 'react'
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+
 function App() {
-
-
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Watch Store</h1>
+    <div className="min-h-screen flex flex-col">
+      {/* Header Section */}
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-3xl font-bold underline text-center">Watch Store</h1>
+      </header>
+
+      {/* Main Content Section */}
+      <main className="flex-grow p-4">
+        <p className="text-center">Welcome to the Watch Store! Explore our collection of premium watches.</p>
+        {/* Add more content here */}
+      </main>
+
+      {/* Footer Section */}
       <Footer />
     </div>
-  )
-}   
+  );
+}
 
-export default App
-
+export default App;
